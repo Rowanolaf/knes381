64e374f21de3e9464ab046069e1fb38bf8318aa6
Rowan Olafson <rowan.olafson@ucalgary.ca>
2024-03-11 21:15:21 -0600

    commiting python stuff

---
+++ RocketChallenge.py
@@ -0,0 +1,33 @@
+# Rocket Challenge code
+
+from time import sleep
+
+def RocketLaunch():
+	print("""
+
+
+
+                   
+                     
+                    /\
+                   /__\
+                  /____\
+                  |    |
+                  |    |
+                  | () |
+                 /|    |\
+		  | __ | 
+		  |(__)|
+	         // \/ \\		
+                //      \\
+               ||        || 
+                             
+                               ''')
+RocetLaunch()
+
+delay = 300
+
+for i in range(60):
+	print()
+	sleep(delay/1000)
+ 	delay = delay* 0.9
